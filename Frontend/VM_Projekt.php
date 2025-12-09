2e9968c28904505b193079ee1a8c355faacb52e5
puska-xyz <lukailikj8@gmail.com>
2025-12-09 13:44:59 +0100

    html geändert

--- Frontend/VM_Projekt.php
+++ Frontend/VM_Projekt.php
@@ -7,7 +7,7 @@
 <style>
     body {
         font-family: system-ui, sans-serif;
-        background: white;  /*#0f172a;*/
+        background: #0f172a;
         color: #e5e7eb;
         margin: 0;
         padding: 0;
@@ -159,102 +159,138 @@
         </div>
     </main>
 
-<div class="server">
-    <!-- SMALL -->
-    <div class="small">
-        <h2 class="smallText">Small</h2>
-        <div class="diagrammSmall">
-            <div class="bar cpu" style="height:75%;">75%</div>
-            <div class="bar ram" style="height:50%;">50%</div>
-            <div class="bar ssd" style="height:30%;">30%</div>
-        </div>
-        <div class="diagrammText">
-            <h4 class="cpu">CPU: </h4>
-            <h4 class="ram">RAM: </h4>
-            <h4 class="ssd">SSD: </h4>
-        </div>
-    </div>
+    <div class="grid">
+        <div class="server">
+            <!-- SMALL -->
+            <div class="small">
+                <h2 class="smallText">Small</h2>
+                <div class="diagrammSmall">
+                    <div class="bar cpu" style="height:75%;">75%</div>
+                    <div class="bar ram" style="height:50%;">50%</div>
+                    <div class="bar ssd" style="height:30%;">30%</div>
+                </div>
+                <div class="diagrammText">
+                    <h4 class="cpu">CPU: </h4>
+                    <h4 class="ram">RAM: </h4>
+                    <h4 class="ssd">SSD: </h4>
+                </div>
+            </div>
 
-    <!-- MEDIUM -->
-    <div class="medium">
-        <h2 class="mediumText">Medium</h2>
-        <div class="diagrammMedium">
-            <div class="bar cpu" style="height:60%;">60%</div>
-            <div class="bar ram" style="height:40%;">40%</div>
-            <div class="bar ssd" style="height:80%;">80%</div>
-        </div>
-        <div class="diagrammText">
-            <h4 class="cpu">CPU: </h4>
-            <h4 class="ram">RAM: </h4>
-            <h4 class="ssd">SSD: </h4>
-        </div>
-    </div>
+            <!-- MEDIUM -->
+            <div class="medium">
+                <h2 class="mediumText">Medium</h2>
+                <div class="diagrammMedium">
+                    <div class="bar cpu" style="height:60%;">60%</div>
+                    <div class="bar ram" style="height:40%;">40%</div>
+                    <div class="bar ssd" style="height:80%;">80%</div>
+                </div>
+                <div class="diagrammText">
+                    <h4 class="cpu">CPU: </h4>
+                    <h4 class="ram">RAM: </h4>
+                    <h4 class="ssd">SSD: </h4>
+                </div>
+            </div>
 
-    <!-- BIG -->
-    <div class="big">
-        <h2 class="bigText">Big</h2>
-        <div class="diagrammBig">
-            <div class="bar cpu" style="height:90%;">90%</div>
-            <div class="bar ram" style="height:70%;">70%</div>
-            <div class="bar ssd" style="height:50%;">50%</div>
+            <!-- BIG -->
+            <div class="big">
+                <h2 class="bigText">Big</h2>
+                <div class="diagrammBig">
+                    <div class="bar cpu" style="height:90%;">90%</div>
+                    <div class="bar ram" style="height:70%;">70%</div>
+                    <div class="bar ssd" style="height:50%;">50%</div>
+                </div>
+                <div class="diagrammText">
+                    <h4 class="cpu">CPU: </h4>
+                    <h4 class="ram">RAM: </h4>
+                    <h4 class="ssd">SSD: </h4>
+                </div>
+            </div>
         </div>
-        <div class="diagrammText">
-            <h4 class="cpu">CPU: </h4>
-            <h4 class="ram">RAM: </h4>
-            <h4 class="ssd">SSD: </h4>
-        </div>
-    </div>
-</div>
 
-<div class="formular">
-    <form action="VM_Projekt.php">
-        <label for="firmenname"></label>
-        <input class="input" type="text" id="firmenname" name="firmenname" placeholder="Firmenname"><br>
-    </form>
+        <!-- VM anlegen -->
+        <div class="card">
+            <h2>Virtuelle Maschine mieten</h2>
+            
+            <form method="post">
+                <label for="vm_name">Name der VM</label>
+                <input type="text" id="vm_name" name="vm_name" placeholder="z.B webserver-01" required>
 
-    <div class="text">
-        <div class="checkboxCpu">
-            <h4>Prozessoren (CPU): gewünschte Anzahl Cores</h4>
-            <form action="VM_Projekt.php">
-                <label><input type="radio" name="cpu" value="1">1 (5 CHF)</label><br>
-                <label><input type="radio" name="cpu" value="2">2 (10 CHF)</label><br>
-                <label><input type="radio" name="cpu" value="4">4 (18 CHF)</label><br>
-                <label><input type="radio" name="cpu" value="8">8 (30 CHF)</label><br>
-                <label><input type="radio" name="cpu" value="16">16 (45 CHF)</label><br>
+                <label for="cpu">CPU (Cores)</label>
+                <select name="cpu" id="cpu" required>
+                    <option value="">Bitte wählen</option>
+                    <!-- do kann meh au eh foreach loop mache -->
+                    <option value="1">1 Cores (5CHF / Monat)</option>
+                    <option value="1">2 Cores (5CHF / Monat)</option>
+                    <option value="1">4 Cores (5CHF / Monat)</option>
+                    <option value="1">8 Cores (5CHF / Monat)</option>
+                    <option value="1">16 Cores (5CHF / Monat)</option>
+                </select>
+
+                <label for="cpu">CPU (Cores)</label>
+                <select name="cpu" id="cpu" required>
+                    <option value="">Bitte wählen</option>
+                    <!-- do kann meh au eh foreach loop mache -->
+                    <option value="1">1 Cores (5CHF / Monat)</option>
+                    <option value="1">2 Cores (5CHF / Monat)</option>
+                    <option value="1">4 Cores (5CHF / Monat)</option>
+                    <option value="1">8 Cores (5CHF / Monat)</option>
+                    <option value="1">16 Cores (5CHF / Monat)</option>
+                </select>
+
+                <label for="cpu">CPU (Cores)</label>
+                <select name="cpu" id="cpu" required>
+                    <option value="">Bitte wählen</option>
+                    <!-- do kann meh au eh foreach loop mache -->
+                    <option value="1">1 Cores (5CHF / Monat)</option>
+                    <option value="1">2 Cores (5CHF / Monat)</option>
+                    <option value="1">4 Cores (5CHF / Monat)</option>
+                    <option value="1">8 Cores (5CHF / Monat)</option>
+                    <option value="1">16 Cores (5CHF / Monat)</option>
+                </select>
+
+                <button type="submit">VM erstellen</button>
             </form>
         </div>
 
-        <div class="checkboxRam">
-            <h4>Arbeitsspeicher (RAM): gewünschte Grösse in GB</h4>
-            <form action="VM_Projekt.php">
-                <label><input type="radio" name="ram" value="8">8 (10 CHF)</label><br>
-                <label><input type="radio" name="ram" value="16">16 (20 CHF)</label><br>
-                <label><input type="radio" name="ram" value="32">32 (40 CHF)</label><br>
-                <label><input type="radio" name="ram" value="64">64 (80 CHF)</label><br>
-                <label><input type="radio" name="ram" value="128">128 (160 CHF)</label><br>
+        <!-- VM lösche -->
+        <div class="card">
+            <h2>Virtuelle Maschine löschen</h2>
+            <form method="post">
+                <label for="vm_name_delete">Name der VM</label>
+                <input type="text" id="vm_name_delete" name="vm_name_delete" placeholder="z.B webserver-01" required>
+
+                <button type="submit" class="delete">WM löschen</button>
+
+                <p>
+                    Hinweis: die vm wird gelöscht!!!!!!!!1!!1!
+                </p>
             </form>
         </div>
 
-        <div class="checkboxSsd">
-            <h4>Speicherplatz (SSD): gewünschte Grösse in TB</h4>
-            <form action="VM_Projekt.php">
-                <label><input type="radio" name="ssd" value="8">2 (20 CHF)</label><br>
-                <label><input type="radio" name="ssd" value="16">4 (40 CHF)</label><br>
-                <label><input type="radio" name="ssd" value="32">8 (80 CHF)</label><br>
-                <label><input type="radio" name="ssd" value="64">16 (160 CHF)</label><br>
-                <label><input type="radio" name="ssd" value="128">32 (320 CHF)</label><br>
-            </form>
+        <!-- Liste der VMs -->
+        <div class="card">
+            <h2>Laufende virtuelle Maschinen</h2>
+            <p>keine vms</p>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Name</th>
+                        <th>Name</th>
+                        <th>Name</th>
+                        <th>Name</th>
+                    </tr>
+                </thead>
+            </table>
         </div>
+
+        <!-- Preis -->
+        <div class="card">
+            <h2>Preis</h2>
+        </div>
+         
     </div>
-
-    <form action="VM_Projekt.php">
-        <label><input class="button" type="submit" name="absenden"></label>
-    </form>
-</div>
-
-<div class="preis">
-    <h1>0 CHF / Monat</h1>
-</div>
 
 </body>
 </html>
