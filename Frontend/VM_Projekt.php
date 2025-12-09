cd3a9ba9ac19bd06fde46323789b08760b815d47
Deal06 <elali.dany2009@gmail.com>
2025-12-09 10:06:03 +0100

    kreisdiagramm zu balkediagramm gänderet, frontend fertig fehlt numme no löschfunktion vo de firmene

--- Frontend/VM_Projekt.php
+++ Frontend/VM_Projekt.php
@@ -1,179 +1,228 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        h1 {
-            text-align: center;
-        }
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>UberCloud</title>
+<style>
+    h1 { text-align: center; }
 
-        .small {
-            border: 2px solid black;
-            border-radius: 10px;
-            width: 300px;
-            height: 500px;
-            transition: all 0.5s ease;
-        }
+    .small, .medium, .big {
+        border: 2px solid black;
+        border-radius: 10px;
+        width: 300px;
+        height: 500px;
+        transition: all 0.5s ease;
+    }
 
-        .small:hover{
-            box-shadow: 10px 10px 10px lightblue;
-            transform: scale(1.1);
-        }
+    .small:hover, .medium:hover, .big:hover {
+        box-shadow: 10px 10px 10px lightblue;
+        transform: scale(1.025);
+    }
 
-        .smallText {
-            text-align: center;
-        }
+    .smallText, .mediumText, .bigText {
+        text-align: center;
+    }
 
-        .server {
-            display: flex;
-            flex-direction: row;
-            justify-content: space-around;
-            margin-top: 50px;
-        }
+    .server {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        margin-top: 50px;
+    }
 
-        .medium {
-            border: 2px solid black;
-            border-radius: 10px;
-            width: 300px;
-            height: 500px;
-            transition: all 0.5s ease;
-        }
+    /* -------------------
+       Balkendiagramme
+       ------------------- */
+    .diagrammSmall, .diagrammMedium, .diagrammBig {
+        display: flex;
+        justify-content: space-around;
+        align-items: flex-end;
+        height: 150px;
+        width: 150px;
+        margin-left: 75px;
+        margin-top: 120px;
+    }
 
-        .mediumText {
-            text-align: center;
-        }
+    .bar {
+        width: 30px;
+        border-radius: 5px 5px 0 0;
+        display: flex;
+        justify-content: center;
+        align-items: flex-end;
+        color: white;
+        font-weight: bold;
+        font-size: 12px;
+    }
 
-        .medium:hover{
-            box-shadow: 10px 10px 10px lightblue;
-            transform: scale(1.1);
-        }
+    .bar.cpu { background-color: darkblue; }
+    .bar.ram { background-color: blue; }
+    .bar.ssd { background-color: turquoise; }
 
-        .big {
-            border: 2px solid black;
-            border-radius: 10px;
-            width: 300px;
-            height: 500px;
-            transition: all 0.5s ease;
-        }
+    .diagrammText {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        margin-top: 10px;
+    }
 
-        .bigText {
-            text-align: center;
-        }
+    .cpu { color: darkblue; }
+    .ram { color: blue; }
+    .ssd { color: turquoise; }
 
-        .big:hover{
-            box-shadow: 10px 10px 10px lightblue;
-            transform: scale(1.1);
-        }
+    .formular {
+        margin-top: 30px;
+        border: 2px solid black;
+        border-radius: 10px;
+        width: 1095px;
+        height: 300px;
+        margin-left: 55px;
+        padding: 30px;
+        transition: all 0.5s ease;
+    }
 
-        .diagrammSmall {
-            width: 150px;
-            height: 150px;
-            border-radius: 50%;
-            background: conic-gradient(darkblue 0% 30%, blue 30% 60%, turquoise 60% 100%); /* Platzhalter */
-            transition: background 0.5s ease; /* optional für sanfte Animation */
-            display: flex;
-            margin-left: 75px;
-            margin-top: 120px;
-        }
+    .formular:hover {
+        box-shadow: 10px 10px 10px lightblue;
+        transform: scale(1.025)
+    }
 
-        .diagrammMedium {
-            width: 150px;
-            height: 150px;
-            border-radius: 50%;
-            background: conic-gradient(darkblue 0% 30%, blue 30% 60%, turquoise 60% 100%); /* Platzhalter */
-            transition: background 0.5s ease; /* optional für sanfte Animation */
-            display: flex;
-            margin-left: 75px;
-            margin-top: 120px;
-        }
+    .input {
+        width: 1070px;
+        height: 50px;
+        font-size: 20px;
+    }
 
-        .diagrammBig {
-            width: 150px;
-            height: 150px;
-            border-radius: 50%;
-            background: conic-gradient(darkblue 0% 30%, blue 30% 60%, turquoise 60% 100%); /* Platzhalter */
-            transition: background 0.5s ease; /* optional für sanfte Animation */
-            display: flex;
-            margin-left: 75px;
-            margin-top: 120px;
-        }
+    .text {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
 
-        .diagrammText {
-            display: flex;
-            flex-direction: row;
-            justify-content: space-around;
-        }
+    .button {
+        background-color: blue;
+        color: white;
+        height: 50px;
+        width: 400px;
+        display: block;
+        margin: 30px auto 0 auto;
+    }
 
-        .cpu {
-            color: darkblue;
-        }
+    .preis {
+        margin-top: 30px;
+        border: 2px solid black;
+        border-radius: 10px;
+        transition: all 0.5s ease;
+        margin-left: 55px;
+        width: 1150px;
+        height: 100px;
+    }
 
-        .ram {
-            color: blue;
-        }
-
-        .ssd {
-            color: turquoise;
-        }
-
-        .formular {
-            margin-top: 20px;
-            border: 2px solid black;
-            border-radius: 10px;
-            width: 1095px;
-            height: 300px;
-            margin-left: 55px;
-            padding: 30px;
-        }
-
-        .input {
-            width: 1070px;
-            height: 50px;
-            font-size: 20px;
-        }
-
-    </style>
+    .preis:hover {
+        box-shadow: 10px 10px 10px lightblue;
+        transform: scale(1.025);
+    }
+</style>
 </head>
 <body>
-    <h1>Willkommen zur UberCloud</h1>
-    <div class = "server">
-        <div class = "small">
-            <h2 class = "smallText">Small</h2>
-            <div class = "diagrammSmall"></div>
-            <div class = "diagrammText">
-                <h4 class = "cpu">CPU: </h4>
-                <h4 class = "ram">RAM:</h4>
-                <h4 class = "ssd">SSD: </h4>
-            </div>
-        </div>
-        <div class = "medium">
-            <h2 class = "mediumText">Medium</h2>
-            <div class = "diagrammMedium"></div>
-            <div class = "diagrammText">
-                <h4 class = "cpu">CPU: </h4>
-                <h4 class = "ram">RAM:</h4>
-                <h4 class = "ssd">SSD: </h4>
-            </div>
-        </div>
-        <div class = "big">
-            <h2 class = "bigText">Big</h2>
-            <div class = "diagrammBig"></div>
-            <div class = "diagrammText">
-                <h4 class = "cpu">CPU: </h4>
-                <h4 class = "ram">RAM:</h4>
-                <h4 class = "ssd">SSD: </h4>
-            </div>
-        </div>
 
+<h1>Willkommen zur UberCloud</h1>
+
+<div class="server">
+    <!-- SMALL -->
+    <div class="small">
+        <h2 class="smallText">Small</h2>
+        <div class="diagrammSmall">
+            <div class="bar cpu" style="height:75%;">75%</div>
+            <div class="bar ram" style="height:50%;">50%</div>
+            <div class="bar ssd" style="height:30%;">30%</div>
+        </div>
+        <div class="diagrammText">
+            <h4 class="cpu">CPU: </h4>
+            <h4 class="ram">RAM: </h4>
+            <h4 class="ssd">SSD: </h4>
+        </div>
     </div>
 
-    <div class = "formular">
-        <form action = "VM_Projekt.php">
-            <label for = "firmenname"></label>
-            <input class = "input" type = "text" id = "firmenname" name = "firmenname" placeholder = "Firmenname"><br>
-        </form>
+    <!-- MEDIUM -->
+    <div class="medium">
+        <h2 class="mediumText">Medium</h2>
+        <div class="diagrammMedium">
+            <div class="bar cpu" style="height:60%;">60%</div>
+            <div class="bar ram" style="height:40%;">40%</div>
+            <div class="bar ssd" style="height:80%;">80%</div>
+        </div>
+        <div class="diagrammText">
+            <h4 class="cpu">CPU: </h4>
+            <h4 class="ram">RAM: </h4>
+            <h4 class="ssd">SSD: </h4>
+        </div>
     </div>
+
+    <!-- BIG -->
+    <div class="big">
+        <h2 class="bigText">Big</h2>
+        <div class="diagrammBig">
+            <div class="bar cpu" style="height:90%;">90%</div>
+            <div class="bar ram" style="height:70%;">70%</div>
+            <div class="bar ssd" style="height:50%;">50%</div>
+        </div>
+        <div class="diagrammText">
+            <h4 class="cpu">CPU: </h4>
+            <h4 class="ram">RAM: </h4>
+            <h4 class="ssd">SSD: </h4>
+        </div>
+    </div>
+</div>
+
+<div class="formular">
+    <form action="VM_Projekt.php">
+        <label for="firmenname"></label>
+        <input class="input" type="text" id="firmenname" name="firmenname" placeholder="Firmenname"><br>
+    </form>
+
+    <div class="text">
+        <div class="checkboxCpu">
+            <h4>Prozessoren (CPU): gewünschte Anzahl Cores</h4>
+            <form action="VM_Projekt.php">
+                <label><input type="radio" name="cpu" value="1">1 (5 CHF)</label><br>
+                <label><input type="radio" name="cpu" value="2">2 (10 CHF)</label><br>
+                <label><input type="radio" name="cpu" value="4">4 (18 CHF)</label><br>
+                <label><input type="radio" name="cpu" value="8">8 (30 CHF)</label><br>
+                <label><input type="radio" name="cpu" value="16">16 (45 CHF)</label><br>
+            </form>
+        </div>
+
+        <div class="checkboxRam">
+            <h4>Arbeitsspeicher (RAM): gewünschte Grösse in GB</h4>
+            <form action="VM_Projekt.php">
+                <label><input type="radio" name="ram" value="8">8 (10 CHF)</label><br>
+                <label><input type="radio" name="ram" value="16">16 (20 CHF)</label><br>
+                <label><input type="radio" name="ram" value="32">32 (40 CHF)</label><br>
+                <label><input type="radio" name="ram" value="64">64 (80 CHF)</label><br>
+                <label><input type="radio" name="ram" value="128">128 (160 CHF)</label><br>
+            </form>
+        </div>
+
+        <div class="checkboxSsd">
+            <h4>Speicherplatz (SSD): gewünschte Grösse in TB</h4>
+            <form action="VM_Projekt.php">
+                <label><input type="radio" name="ssd" value="8">2 (20 CHF)</label><br>
+                <label><input type="radio" name="ssd" value="16">4 (40 CHF)</label><br>
+                <label><input type="radio" name="ssd" value="32">8 (80 CHF)</label><br>
+                <label><input type="radio" name="ssd" value="64">16 (160 CHF)</label><br>
+                <label><input type="radio" name="ssd" value="128">32 (320 CHF)</label><br>
+            </form>
+        </div>
+    </div>
+
+    <form action="VM_Projekt.php">
+        <label><input class="button" type="submit" name="absenden"></label>
+    </form>
+</div>
+
+<div class="preis">
+    <h1>0 CHF / Monat</h1>
+</div>
+
 </body>
 </html>
